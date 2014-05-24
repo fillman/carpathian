43de2e98fedb87e180bea7ab3d54aa3d6dfd1ff1
katerina bobyr <katia.bobyr@gmail.com>
2014-05-24 17:28:41 +0300

    add slowHide method to Fader prototype

--- app/assets/javascripts/fader.js
+++ app/assets/javascripts/fader.js
@@ -18,10 +18,26 @@ $(function(){
     });
   };
 
+  Fader.prototype.slowhide = function() {
+      var docViewTop = $(window).scrollTop(),
+      	elemTop = this.$element.offset().top,
+      	hidingStart = elemTop,
+      	elemBottom = elemTop + this.$element.height(),
+      	alpha = (docViewTop - hidingStart) / (elemBottom - hidingStart),
+      	opacity_value = 1 - alpha;
+      if (hidingStart < docViewTop) { 
+    	  this.$element.css('opacity', opacity_value);  
+      } else { 
+    	  this.$element.css('opacity', '1');
+      }
+  };
+  
   $('[data-ride="fader"]').each(function () {
     var $element = $(this),
         fader = new Fader($element, {});
 
     setInterval(function() { fader.fade() }, {});
+    $(window).scroll(function(){ fader.slowhide(); })
+    
   })
 });
