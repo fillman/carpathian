424820b4322e97fcfceffc4f5d8c9d4b0154a993
fillman <fila.luka@gmail.com>
2014-05-29 21:13:47 +0300

    a lot of fixes with styling using BEM

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -12,21 +12,21 @@ $(function(){
 
   // Adding a fade method
   Category.prototype.showCategory = function(event) {
-    $(this.$element).find('a.category').stop().animate({
+    $(this.$element).find('.carousel-category').stop().animate({
       top: '5%'
     }, 200);
 
-    $(this.$element).find('.item-pattern-black').stop().animate({
+    $(this.$element).find('.carousel-pattern-black').stop().animate({
       opacity: '0.6'
     }, 200);
   };
 
   Category.prototype.hideCategory = function(event) {
-    $(this.$element).find('a.category').stop().animate({
+    $(this.$element).find('.carousel-category').stop().animate({
       top: '-15%'
     }, 200);
 
-    $(this.$element).find('.item-pattern-black').stop().animate({
+    $(this.$element).find('.carousel-pattern-black').stop().animate({
       opacity: '0'
     }, 200);
   };
